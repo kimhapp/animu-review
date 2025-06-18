0606dc11941c6b1a011cdc322ac8fbf5c4716bff
Gaz Dragon <kimhapp.huy@gmail.com>
2025-06-19 01:27:12 +0700

    .

--- app/Http/Controllers/CreateReviewController.php
+++ app/Http/Controllers/CreateReviewController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Review;
+use App\Notifications\ReviewPosted;
+use App\Http\Requests\Admin\ReviewRequest;
 use Inertia\Inertia;
 
 class CreateReviewController extends Controller
@@ -10,5 +13,13 @@ class CreateReviewController extends Controller
     //
     public function create(Request $request) {
         return Inertia::render('create-review');
+    }
+
+    public function store(ReviewRequest $request, Review $review) {
+        $followers = $review->user->followers;
+
+        foreach ($followers as $follower) {
+            $follower->notify(new ReviewPosted($review));
+        }
     }
 }
---
+++ app/Notifications/ReviewPosted.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Review;
+
+class ReviewPosted extends Notification
+{
+    use Queueable;
+
+    protected $review;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Review $review)
+    {
+        $this->review = $review;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable)
+    {
+        $url = route('reviews.show', $this->review->id);
+
+        return (new MailMessage)
+            ->subject('New Review Posted')
+            ->greeting("Hi {$notifiable->name},")
+            ->line("{$this->review->user->name} posted a new review.")
+            ->action('View Review', $url)
+            ->line('Thank you for using our app!');
+    }
+    
+    public function toDatabase($notifiable)
+    {
+        return [
+            'review_id' => $this->review->id, 
+            'reviewer_id' => $this->review->user_id,
+            'reviewer_name' => $this->review->user->name,
+            'anime_id' => $this->review->anime_id,
+            'message' => "{$this->review->user->name} posted a new review.",
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
